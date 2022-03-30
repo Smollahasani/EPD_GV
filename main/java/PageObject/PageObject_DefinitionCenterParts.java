0621085bc03a3d3549a73d31c05a71b06a6dc0d0
samane.mollahasani <s.mollahasani@gmail.com>
2022-03-30 15:09:57 +0430

    10far

--- main/java/PageObject/PageObject_DefinitionCenterParts.java
+++ main/java/PageObject/PageObject_DefinitionCenterParts.java
@@ -2,6 +2,7 @@ package PageObject;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,7 +17,9 @@ public class PageObject_DefinitionCenterParts {
 
   element_Highlight eh= new element_Highlight();
   
-  String AddedCode; 
+  String PartName;
+  String AddedNewPartName;
+  String AddedNewPartCode;
   String InsuranceName;
 
    @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[2]/div/form/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]")
@@ -52,6 +55,9 @@ public class PageObject_DefinitionCenterParts {
    @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[1]/form/div/div[2]/mat-form-field/div/div[1]/div/mat-select/div/div[1]")
    WebElement Group;
    
+   @FindBy(xpath="/html/body/div[2]/div[2]/div/div/div/mat-option[6]/span")
+   WebElement Group_Clinic;
+   
    @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[1]/form/div/div[3]/mat-form-field/div/div[1]/div/input")
    WebElement Name;
    
@@ -60,6 +66,16 @@ public class PageObject_DefinitionCenterParts {
    
    @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/thead/tr/th[5]/mat-checkbox/label/span[1]")
    WebElement Standardization;
+   
+   @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/tbody/tr/td[5]/mat-form-field/div/div[1]/div/mat-select")
+   WebElement StandardizationCombo;
+   
+   @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/tbody/tr/td[2]")
+   WebElement FirstNametd;
+   
+   @FindBy(xpath="/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/tbody/tr/td[4]")
+   WebElement FirstCodetd;
+
 
 
    PageObject_DefinitionCenterParts DeActive =new PageObject_DefinitionCenterParts();
@@ -83,19 +99,78 @@ public class PageObject_DefinitionCenterParts {
    }
    
 //اضافه کردن بخش
-   public void AddCenterParts( WebDriver driver ,String insuranceCode ,String insuranceCode2 ,String insuranceCode3 ) throws InterruptedException {
+   public void AddCenterParts( WebDriver driver ,String CenterPartName ,String CenterPartCode ) throws InterruptedException {
 	   
 	   Actions action = new Actions(driver);
-	   
-	   
+	   action
+	   .click(Activity)
+	   .sendKeys(Keys.ENTER)
+	   .click(Group)
+	   .click(Group_Clinic)
+	   .click(Name)
+	   .sendKeys(CenterPartName)
+	   .click(Code)
+	   .sendKeys(CenterPartCode)
+	   .click(AddButton)
+	   .perform();
+	  	   
    }
- //سرچ نام بخش
-   public void SearchCenterParts ( WebDriver driver ) throws InterruptedException {
+ //ویرلیش نام بخش
+   public void EditCenterPartName ( WebDriver driver , String NewName) throws InterruptedException {
 	   
 	   Actions action = new Actions(driver);
+	   WebElement PenButton =driver.findElement(By.xpath("/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/tbody/tr/td[6]/button"));
+	   action
+	   .click(PenButton)
+	   .click(Name)
+	   .sendKeys(Keys.CLEAR)
+	   .click(Name)
+	   .sendKeys(NewName)
+	   .perform();
+	   WebElement EditButton =driver.findElement(By.xpath("/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[1]/form/div/div[5]/button[1]"));
+	   EditButton.click();
+	   WebElement POPUP =driver.findElement(By.xpath("/div/div[1]"));
+	   String c=POPUP.getText();
+       Assert.assertTrue(c.contains("موفقیت"));
+	   //چک ثبت ویرایش
+	   AddedNewPartName=FirstNametd.getText();
+	   Assert.assertEquals(AddedNewPartName, NewName);
+	   
    }   
-   //غیر فعال کردن بخش
-   public void DeActiveCenterParts ( WebDriver driver ,String insuranceCode ) throws InterruptedException {
+   //ویرلیش کد بخش
+   public void EditCenterPartCode ( WebDriver driver , String NewCode) throws InterruptedException {
+	   
 	   Actions action = new Actions(driver);
+	   WebElement PenButton =driver.findElement(By.xpath("/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[3]/table/tbody/tr/td[6]/button"));
+	   action
+	   .click(PenButton)
+	   .click(Code)
+	   .sendKeys(Keys.CLEAR)
+	   .click(Code)
+	   .sendKeys(NewCode)
+	   .perform();
+	   WebElement EditButton =driver.findElement(By.xpath("/html/body/app-root/div[2]/div[2]/div/div/app-definition-center-parts/div/mat-card/mat-card-content/div[1]/form/div/div[5]/button[1]"));
+	   EditButton.click();
+	   WebElement POPUP =driver.findElement(By.xpath("/div/div[1]"));
+	   String c=POPUP.getText();
+       Assert.assertTrue(c.contains("موفقیت"));
+	   //چک ثبت ویرایش
+	   AddedNewPartCode=FirstCodetd.getText();
+	   Assert.assertEquals(AddedNewPartName, NewCode); 
+}
+   //استانداردسازی  
+   public void Standardization ( WebDriver driver  ) throws InterruptedException {
+	   
+	   Actions action = new Actions(driver);
+	   action
+	   .click(Standardization)
+	   .click(StandardizationCombo)
+	   .sendKeys(Keys.ENTER)
+	   .perform();
+	   WebElement POPUP =driver.findElement(By.xpath("/div/div[1]"));
+	   String c=POPUP.getText();
+       Assert.assertTrue(c.contains("موفقیت"));
+
+	   
    }
  }
